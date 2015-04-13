2fa1f7bf1c20146662b493e6bca6ea0ecd60e0bc
vitaliBazhko <vitali_bazhko@epam.com>
2015-04-13 16:09:26 +0300

    Created new branch for HW tasks

---
+++ vitali_bazhko/src/lab1/App.java
@@ -0,0 +1,10 @@
+package lab1;
+
+/**
+ * Created by Vitali_Bazhko on 4/13/2015.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Created new branch for HW tasks");
+    }
+}
