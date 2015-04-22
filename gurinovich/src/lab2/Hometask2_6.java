b21765c3deecfdc879dedf0d42c5f9e8e300effd
vgurinovich87 <tory687@gmail.com>
2015-04-22 20:19:53 +0300

    Hometask2 exercise 6 unified

--- gurinovich/src/lab2/Hometask2_6.java
+++ gurinovich/src/lab2/Hometask2_6.java
@@ -15,17 +15,22 @@ public class Hometask2_6 {
         }
 
         double number_2 = 43.097897;
-        int last_2 = 0;
         String s2 = String.valueOf(number_2);
+        int last_2 = Integer.parseInt(s2.substring(s2.length() - 1, s2.length()));
+        if (last_2 == 7){
+            System.out.println("Last digit of Number2 is 7");
+        } else {
+        System.out.println("Last digit of Number2 is not 7 ! It is " + last_2);
+        }
         //String s2 = Double.toString(number_2);
-        String s1 = String.valueOf((int)number_2);
+        /*String s1 = String.valueOf((int)number_2);
         int length = s2.length() - s1.length() - 1;
 
         for (int i=0; i<length; i++) {
             number_2 -= (int)number_2;
             BigDecimal x = new BigDecimal(number_2);
             x = x.setScale(length, BigDecimal.ROUND_HALF_UP); /* length - количество знаков после запятой */
-            number_2 = x.doubleValue();
+            /*number_2 = x.doubleValue();
             number_2 *= 10;
             last_2 = (int)number_2;
         }
@@ -34,6 +39,6 @@ public class Hometask2_6 {
             System.out.println("Last digit of Number2 is 7");
         } else {
             System.out.println("Last digit of Number2 is not 7 ! It is " + last_2);
-        }
+        }*/
     }
 }
