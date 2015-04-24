8e789f52584b5bfdbd885acde34165d67539898a
Andrei_Karzhou <karzhouandrew@gmail.com>
2015-04-24 10:50:32 +0200

    Commit 1

--- korzhov/src/lab1/HelloWorld.java
+++ korzhov/src/lab1/HelloWorld.java
@@ -3,7 +3,7 @@ package lab1;
 // Hello world program
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("lab1");
     }
 
 }
