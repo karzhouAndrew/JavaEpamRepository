9958d53a143b9ca16821a896e791f260b8d86d05
Vitali <vitaly2003@gmail.com>
2015-04-26 19:04:02 +0300

    Fixed existing issues in task

--- vitali_bazhko/src/HW3/Exercise4.java
+++ vitali_bazhko/src/HW3/Exercise4.java
@@ -9,27 +9,24 @@ package HW3;
 public class Exercise4 {
     public static void main(String[] args) {
         int arr[][] = new int[5][5];
-        int i, j;
 
-        for (i = 0; i < arr.length / 2 + 1; i++) {
-            for (j = 0; j < arr[i].length; j++) {
-                if ((j < i) || (j >= (arr[i].length - i)))
-                    arr[i][j] = 0;
-                else
+        for (int i = 0; i < arr.length / 2 + 1; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if ((j < i) || (j >= (arr[i].length - i))) {
                     arr[i][j] = 1;
+                }
             }
         }
-        for (i = arr.length - 1; i >= arr.length / 2 + 1; i--) {
-            for (j = 0; j < arr[i].length; j++) {
-                if ((j < (arr[i].length - 1 - i)) || (j > i))
-                    arr[i][j] = 0;
-                else
+        for (int i = arr.length - 1; i >= arr.length / 2 + 1; i--) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if ((j < (arr[i].length - 1 - i)) || (j > i)) {
                     arr[i][j] = 1;
+                }
             }
         }
-        for (i = 0; i < arr.length; i++) {
-            for (j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j]);
+        for (int[] anArr : arr) {
+            for (int j = 0; j < anArr.length; j++) {
+                System.out.print(anArr[j]);
             }
             System.out.println(" ");
         }
