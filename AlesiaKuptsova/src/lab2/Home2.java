b3979604fa37f81aefc21e80dcd9a5b925f73b22
Alesya_Kuptsova <alesya.kuptsova@tut.by>
2015-05-02 23:20:40 +0300

    lab2

---
+++ AlesiaKuptsova/src/lab2/Home2.java
@@ -0,0 +1,28 @@
+/*
+Дано  четырехзначное натуральное  n. Верно ли, что все  цифры числа различны?
+ */
+
+import java.util.Scanner;
+
+public class Home2 {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int num;
+		int b;
+		int x;
+		int c;
+		int d;
+		System.out.print ("Введите число=");
+		num = in.nextInt();
+		in.close();
+		b=num/1000; 
+		d=num%1000/100; 
+		c=num%100/10; 
+		x=num%10;
+		if ((b!=d) && (d!=c) &&(c!=x) && (b!=c) && (b!=x) && (d!=x)){
+			System.out.println("Верно");
+		}else{
+			System.out.println("Не верно");
+		}
+	}
+}
