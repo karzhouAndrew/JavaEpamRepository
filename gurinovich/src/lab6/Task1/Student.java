dc72eb23d486450b6d24fa8fc1da3ee05edc8085
vgurinovich87 <tory687@gmail.com>
2015-05-04 22:43:50 +0300

    Without Lab4 :)

---
+++ gurinovich/src/lab6/Task1/Student.java
@@ -0,0 +1,34 @@
+package lab6.Task1;
+
+public class Student {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private int mark;
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+}
+
